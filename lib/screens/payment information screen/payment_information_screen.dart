772a3c12b810d6a0745f822144322d7176ce2bac
AthulprakashQMP <athulprakash@qmpglobal.in>
2023-09-07 15:51:43 +0530

    refactor the widgets

--- lib/screens/payment information screen/payment_information_screen.dart
+++ lib/screens/payment information screen/payment_information_screen.dart
@@ -2,7 +2,9 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:sitare/constants/ui_constants.dart';
 
+import 'widgets/coupon_code_widget.dart';
 import 'widgets/payment_details_row_widget.dart';
+import 'widgets/payment_details_widget.dart';
 
 class PaymentScreen extends StatelessWidget {
   PaymentScreen({super.key});
@@ -18,7 +20,7 @@ class PaymentScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: PRIMARY_COLOR,
         title: const Text(
-          'Payment Infromation',
+          'Payment Information',
           style: TextStyle(
             fontWeight: FontWeight.bold,
             color: whiteColor,
@@ -27,199 +29,8 @@ class PaymentScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          SizedBox(
-            child: Padding(
-              padding: EdgeInsets.all(size.width / 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    'Payment details:',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  PaymentDetailsRowWidget(
-                      amount: totalAmount, text: 'Total Amount'),
-                  const Divider(
-                    thickness: 2,
-                    color: greyColor,
-                  ),
-                  PaymentDetailsRowWidget(amount: gstAmount, text: 'GST @ 18%'),
-                  const Divider(
-                    thickness: 2,
-                    color: greyColor,
-                  ),
-                  PaymentDetailsRowWidget(
-                      amount: payableAmount, text: 'Total Amount'),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            color: Color.fromARGB(255, 4, 41, 72),
-            // height: 100,
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(size.width / 16, size.width / 16,
-                  size.width / 16, size.width / 16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Coupon code',
-                    style: TextStyle(
-                        color: whiteColor,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        width: (size.width * .7) - size.width / 7,
-                        child: TextFormField(
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            fillColor: whiteColor,
-                            filled: true,
-                          ),
-                        ),
-                      ),
-                      GestureDetector(
-                        onTap: () {},
-                        child: Container(
-                          width: size.width * .3,
-                          height: 50,
-                          color: redColor,
-                          child: Center(
-                            child: AutoSizeText(
-                              'Apply',
-                              maxLines: 1,
-                              style: TextStyle(
-                                  fontSize: 16,
-                                  color: whiteColor,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  GestureDetector(
-                      onTap: () {
-                        showModalBottomSheet(
-                          constraints: BoxConstraints.expand(
-                              height: size.height * .6, width: size.width),
-                          isScrollControlled: true,
-                          context: context,
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(20),
-                              topRight: Radius.circular(20),
-                            ),
-                          ),
-                          // clipBehavior: Clip.hardEdge,
-                          // barrierColor: Colors.red,
-                          // enableDrag: false,
-                          builder: (context) {
-                            return Padding(
-                              padding: EdgeInsets.all(size.width / 16),
-                              child: SingleChildScrollView(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.end,
-                                      children: [
-                                        GestureDetector(
-                                          onTap: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: Icon(Icons.close)),
-                                      ],
-                                    ),
-                                    Text(
-                                      'Coupon code',
-                                      style: TextStyle(
-                                          color: blackColor,
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16),
-                                    ),
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        SizedBox(
-                                          height: 50,
-                                          width:
-                                              (size.width * .7) - size.width / 7,
-                                          child: TextFormField(
-                                            
-                                            decoration: InputDecoration(
-                                              hintText: 'Enter Coupon Code',
-                                              border: OutlineInputBorder(
-                                                  borderSide: BorderSide(
-                                                      color: greenColor)),
-                                            ),
-                                          ),
-                                        ),
-                                        GestureDetector(
-                                          onTap: () {},
-                                          child: Container(
-                                            width: size.width * .3,
-                                            height: 50,
-                                            color: redColor,
-                                            child: Center(
-                                              child: AutoSizeText(
-                                                'Apply',
-                                                maxLines: 1,
-                                                style: TextStyle(
-                                                    fontSize: 16,
-                                                    color: whiteColor,
-                                                    fontWeight: FontWeight.bold),
-                                              ),
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      height: 30,
-                                    ),
-                                    SizedBox(
-                                      width: size.width,
-                                      child: Text( """Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-                                                        """,style: TextStyle(color: greyColor),),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            );
-                          },
-                        );
-                      },
-                      child: Text(
-                        'Apply another coupon',
-                        style: TextStyle(
-                            color: redColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                            decoration: TextDecoration.underline,
-                            decorationColor: redColor),
-                      )),
-                ],
-              ),
-            ),
-          ),
+          PaymentDetailsWidget(size: size, totalAmount: totalAmount, gstAmount: gstAmount, payableAmount: payableAmount),
+          CouponCodeWidget(size: size),
           SizedBox(
             height: 20,
           ),
