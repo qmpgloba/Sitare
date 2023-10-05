ff4a5accd20b54d606ed5c9c12c7a447cfe8f91a
AthulprakashQMP <athulprakash@qmpglobal.in>
2023-10-05 12:43:59 +0530

    phone number issue fixed

--- lib/functions/get_user_details.dart
+++ lib/functions/get_user_details.dart
@@ -2,9 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<DocumentSnapshot<Map<String,dynamic>>?> getUserDataByPhoneNumber (String phoneNumber) async{
 
-String number = "+91$phoneNumber";
+// String number = "+91$phoneNumber";
   final userColection = FirebaseFirestore.instance.collection('users');
-  final querySnapShot = await userColection.where('phone number',isEqualTo: number).limit(1).get();
+  final querySnapShot = await userColection.where('phone number',isEqualTo: phoneNumber).limit(1).get();
   if(querySnapShot.docs.isNotEmpty){
     return querySnapShot.docs.first;
   }else{
