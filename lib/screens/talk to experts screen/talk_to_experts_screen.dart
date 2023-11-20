ac90bebcd0e5084cb8c9cc81d18c711fca06076e
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-11-20 17:34:45 +0530

    shimmer file created

--- lib/screens/talk to experts screen/talk_to_experts_screen.dart
+++ lib/screens/talk to experts screen/talk_to_experts_screen.dart
@@ -53,166 +53,168 @@ class _TalkToExpertsScreenState extends State<TalkToExpertsScreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.sizeOf(context);
     return Scaffold(
-        backgroundColor: whiteColor,
-        appBar: AppBar(
-          backgroundColor: PRIMARY_COLOR,
-          title: const Text(
-            'Talk to Experts',
-            style: TextStyle(color: whiteColor, fontWeight: FontWeight.bold),
-          ),
-          bottom: PreferredSize(
-              preferredSize: _isVisible == true
-                  ? Size(size.width, size.width * .28)
-                  : Size(size.width, size.width * .1),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(size.width / 20),
-                    child: Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) =>
-                                  const WalletRechargeScreen(),
-                            ));
-                          },
-                          child: const Icon(
-                            Icons.account_balance_wallet_outlined,
-                            color: whiteColor,
-                            size: 25,
-                          ),
+      backgroundColor: whiteColor,
+      appBar: AppBar(
+        backgroundColor: PRIMARY_COLOR,
+        title: const Text(
+          'Talk to Experts',
+          style: TextStyle(color: whiteColor, fontWeight: FontWeight.bold),
+        ),
+        bottom: PreferredSize(
+            preferredSize: _isVisible == true
+                ? Size(size.width, size.width * .28)
+                : Size(size.width, size.width * .1),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(size.width / 20),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const WalletRechargeScreen(),
+                          ));
+                        },
+                        child: const Icon(
+                          Icons.account_balance_wallet_outlined,
+                          color: whiteColor,
+                          size: 25,
                         ),
-                        const SizedBox(
-                          width: 5,
-                        ),
-                        Text(
-                          '₹$walletAmmount',
-                          style:
-                              const TextStyle(color: whiteColor, fontSize: 16),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) =>
-                                  const WalletRechargeScreen(),
-                            ));
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: redColor,
-                                borderRadius: BorderRadius.circular(3)),
-                            child: const Padding(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 5, vertical: 2),
-                              child: AutoSizeText(
-                                'RECHARGE',
-                                maxLines: 1,
-                                style: TextStyle(
-                                    fontSize: 14,
-                                    color: whiteColor,
-                                    fontWeight: FontWeight.bold),
-                              ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        '₹$walletAmmount',
+                        style: const TextStyle(color: whiteColor, fontSize: 16),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const WalletRechargeScreen(),
+                          ));
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: redColor,
+                              borderRadius: BorderRadius.circular(3)),
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 5, vertical: 2),
+                            child: AutoSizeText(
+                              'RECHARGE',
+                              maxLines: 1,
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: whiteColor,
+                                  fontWeight: FontWeight.bold),
                             ),
                           ),
                         ),
-                        const Spacer(),
-                        GestureDetector(
-                          onTap: () {
-                            // Navigator.of(context).push(MaterialPageRoute(builder: (context) => FilterSectionSheet(),));
-                            filterSectionBottomSheet(size, context);
-                          },
-                          child: const Icon(
-                            Icons.filter_list,
-                            size: 30,
-                            color: greyColor,
-                          ),
+                      ),
+                      const Spacer(),
+                      GestureDetector(
+                        onTap: () {
+                          // Navigator.of(context).push(MaterialPageRoute(builder: (context) => FilterSectionSheet(),));
+                          filterSectionBottomSheet(size, context);
+                        },
+                        child: const Icon(
+                          Icons.filter_list,
+                          size: 30,
+                          color: greyColor,
                         ),
-                        const SizedBox(
-                          width: 10,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _isVisible = !_isVisible;
+                          });
+                        },
+                        child: const Icon(
+                          Icons.search,
+                          size: 30,
+                          color: greyColor,
                         ),
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              _isVisible = !_isVisible;
-                            });
-                          },
-                          child: const Icon(
-                            Icons.search,
-                            size: 30,
-                            color: greyColor,
-                          ),
-                        )
-                      ],
-                    ),
+                      )
+                    ],
                   ),
-                  Visibility(
-                    visible: _isVisible,
-                    child: Container(
-                      color: whiteColor,
-                      width: size.width,
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: size.width / 20, vertical: 5),
-                        child: TextFormField(
-                          keyboardType: TextInputType.name,
-                          controller: _searchTextController,
-                          onChanged: (value) {
-                            setState(() {});
-                          },
-                          style: const TextStyle(color: blackColor),
-                          decoration: InputDecoration(
-                              hintText: 'Search...',
-                              hintStyle: const TextStyle(color: FONT_COLOR),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(40))),
-                        ),
+                ),
+                Visibility(
+                  visible: _isVisible,
+                  child: Container(
+                    color: whiteColor,
+                    width: size.width,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: size.width / 20, vertical: 5),
+                      child: TextFormField(
+                        keyboardType: TextInputType.name,
+                        controller: _searchTextController,
+                        onChanged: (value) {
+                          setState(() {});
+                        },
+                        style: const TextStyle(color: blackColor),
+                        decoration: InputDecoration(
+                            hintText: 'Search...',
+                            hintStyle: const TextStyle(color: FONT_COLOR),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40))),
                       ),
                     ),
                   ),
-                ],
-              )),
-        ),
-        body: FutureBuilder<List<AstrologerModel>>(
-          future: fetchFilteredAstrologersFromFirestore(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError) {
-              return Center(
-                child: Text(snapshot.error.toString()),
-              );
+                ),
+              ],
+            )),
+      ),
+      body: FutureBuilder<List<AstrologerModel>>(
+        future: fetchFilteredAstrologersFromFirestore(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text(snapshot.error.toString()),
+            );
+          } else {
+            List<AstrologerModel>? astrologers = snapshot.data;
+            List<AstrologerModel> filteredAstrologers = [];
+
+            if (_searchTextController.text.isNotEmpty) {
+              filteredAstrologers = astrologers!
+                  .where((astrologer) => astrologer.fullName
+                      .toLowerCase()
+                      .contains(_searchTextController.text.toLowerCase()))
+                  .toList();
             } else {
-              List<AstrologerModel>? astrologers = snapshot.data;
-              List<AstrologerModel> filteredAstrologers = [];
-
-              if (_searchTextController.text.isNotEmpty) {
-                filteredAstrologers = astrologers!
-                    .where((astrologer) => astrologer.fullName
-                        .toLowerCase()
-                        .contains(_searchTextController.text.toLowerCase()))
-                    .toList();
-              } else {
-                filteredAstrologers = astrologers!;
-              }
-
-              return filteredAstrologers.isEmpty
-                  ? const Center(child: Text('No Astrologers Found'))
-                  : ListView.builder(
-                      // scrollDirection: Axis.horizontal,
-                      itemCount: filteredAstrologers.length,
-                      itemBuilder: (context, index) =>
-                          TalkToExpertsProfileDetailsWidget(
-                              size: size,
-                              astrologer: filteredAstrologers[index]));
+              filteredAstrologers = astrologers!;
             }
-          },
-        ));
+
+            return filteredAstrologers.isEmpty
+                ? const Center(
+                    child: Text('No Astrologers Found'),
+                  )
+                : ListView.builder(
+                    // scrollDirection: Axis.horizontal,
+                    itemCount: filteredAstrologers.length,
+                    itemBuilder: (context, index) =>
+                        TalkToExpertsProfileDetailsWidget(
+                      size: size,
+                      astrologer: filteredAstrologers[index],
+                    ),
+                  );
+          }
+        },
+      ),
+    );
   }
 
   Future<dynamic> filterSectionBottomSheet(
---
+++ lib/screens/talk to experts screen/widgets/shimmer/shimmer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TalkToExpertShimmer extends StatefulWidget {
+  const TalkToExpertShimmer({super.key});
+
+  @override
+  State<TalkToExpertShimmer> createState() => _TalkToExpertShimmerState();
+}
+
+class _TalkToExpertShimmerState extends State<TalkToExpertShimmer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
